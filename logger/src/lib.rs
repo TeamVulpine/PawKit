49550faf98103d0854c71ba8bcf5088c8eb45308
Oliver-makes-code <oliver@olivermakesco.de>
2025-10-14 15:33:11 -0500

    Fix badges

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -38,26 +38,26 @@ pub fn set_logger_callbacks(callback: Box<dyn LoggerCallbacks>) {
 
 macro log_badge($prefix: tt, $badge: tt, $start: tt, $msg: tt, $end: tt) {
     let time = time_string();
-    print_to_console(&format!(concat!(ansi!$prefix, "[", $badge, "]", ansi!$start, " {} | {}\n", ansi!$end), time, $msg));
-    print_to_logfile(&format!(concat!("[", $badge, "]", " {} | {}\n"), time, $msg));
+    print_to_console(&format!(concat!(ansi!$prefix, $badge, ansi!$start, " {} | {}\n", ansi!$end), time, $msg));
+    print_to_logfile(&format!(concat!($badge, " {} | {}\n"), time, $msg));
 }
 
 pub fn info(message: &str) {
-    log_badge!((reset, fg_green), "INFO ", (reset), message, (reset));
+    log_badge!((reset, fg_green), "[INFO] ", (reset), message, (reset));
 }
 
 pub fn debug(message: &str) {
-    log_badge!((reset, fg_blue), "DEBUG", (reset), message, (reset));
+    log_badge!((reset, fg_blue), "[DEBUG]", (reset), message, (reset));
 }
 
 pub fn warn(message: &str) {
-    log_badge!((reset, fg_yellow), "WARN ", (reset), message, (reset));
+    log_badge!((reset, fg_yellow), "[WARN] ", (reset), message, (reset));
 }
 
 pub fn error(message: &str) {
-    log_badge!((reset, fg_red), "ERROR", (reset), message, (reset));
+    log_badge!((reset, fg_red), "[ERROR]", (reset), message, (reset));
 }
 
 pub fn fatal(message: &str) {
-    log_badge!((reset, fg_red, bold), "FATAL", (), message, (reset));
+    log_badge!((reset, fg_red, bold), "[FATAL]", (), message, (reset));
 }
